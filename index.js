96f4d0dbab7e5ce6711f35524edefac5fd48a4ad
kayleeburch <kayleemburch@gmail.com>
2022-06-19 11:14:25 -0400

    refresh working

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
-// const getPokeBois = async () => {
-//     try {
-//         let response = await axios.get('https://pokeapi.co/api/v2/pokemon/')
-//         const pokeNum = response.data.count
-//         const randomNum = Math.floor(Math.random() * pokeNum)
-//         response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
-//         console.log(response)
-//         let pokeType = response.data.types[0].type['name']
-//         console.log(pokeType)
-//         let pokeList = []
-//         response = await axios.get(`https://pokeapi.co/api/v2/type/${pokeType}`)
-//         console.log(response)
-        
-//     }
-//     catch(e){
-//         console.log('NOPE, NICE TRY')
-//         console.log(e)
-//     }
-// }
+const getPokeBois = async () => {
+    try {
+        let response = await axios.get('https://pokeapi.co/api/v2/pokemon/')
+        const pokeNum = response.data.count
+        const randomNum = Math.floor(Math.random() * pokeNum)
+        response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
+        let pokeType = response.data.types[0].type['name']
+        response = await axios.get(`https://pokeapi.co/api/v2/type/${pokeType}`)
+        let allTypesNum = response.data.pokemon.length
+        let allTypesArr = response.data.pokemon
+        // console.log(pokeType)
 
-function GFG_Fun() {
-    var img = document.createElement('img');
-    img.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/7.png';
-    document.getElementById('body').appendChild(img);
-    down.innerHTML = "Image Element Added.";
+        let pokeList = []
+        while(pokeList.length < 6){
+            let randomFiveID = Math.floor(Math.random() * allTypesNum)
+            for(let x in allTypesArr){
+                if(x == randomFiveID){
+                    let pokemonUrl = allTypesArr[x].pokemon['url']
+                    response = await axios.get(`${pokemonUrl}`)
+                    let getImage = response.data.sprites.front_default
+                    pokeList.push(getImage)
+                }
+            }
+        }
+        
+
+        document.getElementById('image-list').innerHTML = '';
+
+        for(let images in pokeList){
+            var img = document.createElement('img')
+            img.src = pokeList[images]
+            document.getElementById('image-list').appendChild(img);
+            console.log(pokeType)
+        } 
+
+        
+        
+    }
+    catch(e){
+        console.log('NOPE, NICE TRY')
+        console.log(e)
+    }
 }
 
-getPokeBois()
+
